5991969500f2b35c8be0d5869678c33d4dab1c76
qq-me <nkshv2@gmail.com>
2024-07-27 15:30:54 +0300

    improved

--- torchzero/optim/first_order/gradient_direction_curvature.py
+++ torchzero/optim/first_order/gradient_direction_curvature.py
@@ -23,16 +23,29 @@ class QuadraticGDC(optim.Optimizer):
         max_dist=8.0,
         discard_over=8.0,
         distance_mul=1.0,
+        validate_step = False,
         foreach=True,
-        log_minimum_x=False,
+        debug=False,
     ):
-        """Quadratic gradient direction curvature optimizer.
+        """Quadratic gradient direction curvature optimizer. You have to pass a callable closure that accepts `backward` boolean argument, for example:
+        ```py
+        def closure(backward: bool):
+            preds = model(inputs)
+            loss = loss_fn(preds, targets)
+            if backward:
+                optimizer.zero_grad()
+                loss.backward()
+            return loss
+
+        optimizer.step(closure)
+
+        ```
 
         This does two forward passes and one backward per step, obtaining three points.
         Those points are then used to fit a second order polynomial,
         which represents directional curvature in the direction of antigradient.
         If curvature is positive and not too small, a step is made to the minimum of the polynomial.
-        Otherwise this does one SGD-like step.
+        Otherwise this acts like SGD.
 
         Args:
             params (_type_): _description_
@@ -41,8 +54,9 @@ class QuadraticGDC(optim.Optimizer):
             max_dist (float, optional): Clips the distance this can step towards polynomial minimum. Defaults to 8.0.
             discard_over (float, optional): Discards polynomial if distance to minimum is larger than this, to avoid instability with very small curvatures. Defaults to 8.0.
             distance_mul (float, optional): Multiplies the magnitude step towards polynomial minimum. Defaults to 1.0.
+            validate_step (bool, optional): Does additional forward pass after stepping to minimum and check if loss actually decreased, if it didn't, undoes the step (so only SGD step remains). Defaults to False.
             foreach (bool, optional): Makes it faster. Defaults to True.
-            log_minimum_x (bool, optional): Log distances to minimum to `log` attribute. Defaults to False.
+            debug (bool, optional): Log distances to minimum and validated loss if it is enabled. Defaults to False.
         """
         super().__init__(params, {})
         self.foreach = foreach
@@ -52,15 +66,18 @@ class QuadraticGDC(optim.Optimizer):
         self.lr = lr
         self.distance_mul = distance_mul
 
-        self.log_minimum_x = log_minimum_x
-        self.log = []
+        self.validate_step = validate_step
+
+        self.debug = debug
+        self.log = {}
+        self.current_step = 0
 
 
     @torch.no_grad
     def step(self, closure): # type:ignore # pylint:disable=W0222
         eps = self.lr * self.lr_mul
 
-        with torch.enable_grad(): y:torch.Tensor = closure(backward=True)
+        with torch.enable_grad(): y:torch.Tensor = closure(backward=True).detach().cpu()
 
         all_grads = _foreach.TensorList([], foreach = self.foreach)
         all_params = _foreach.TensorList([], foreach = self.foreach)
@@ -75,15 +92,36 @@ class QuadraticGDC(optim.Optimizer):
 
         # make a big step in the direction of the antigradient
         all_params.sub_(all_grads, alpha = self.lr)
-        y_step = closure(backward=False)
+        y_step = closure(backward=False).detach().cpu()
 
-        if (y - y_epsilon) / eps > (y - y_step) / self.lr:
-            minimum_x = find_minimum_x_quadratic_numpy([0, eps, self.lr], [y, y_epsilon, y_step])
-            if self.log_minimum_x: self.log.append(minimum_x)
+        if self.debug:
+            log = self.log[self.current_step] = {}
+            log['y'] = float(y)
+            log['y_epsilon'] = float(y_epsilon)
+            log['y_step'] = float(y_step)
 
-            minimum_x *= self.distance_mul
-            if minimum_x > self.lr and minimum_x < self.discard_over:
-                if minimum_x > self.max_dist: minimum_x = self.max_dist
-                all_params.sub_(all_grads, alpha = minimum_x - self.lr)
+        # make sure curvature is positive in the direction of gradient
+        if y_step < y and (y - y_epsilon) / eps > (y - y_step) / self.lr:
+            # find distance to minimum
+            minimum_x_dist = find_minimum_x_quadratic_numpy([0, eps, self.lr], [y, y_epsilon, y_step])
+            if self.debug: log['minimum_x_dist'] = float(minimum_x_dist) # type:ignore
 
+            # make sure we don't step back
+            minimum_x_dist *= self.distance_mul
+            if minimum_x_dist > self.lr and minimum_x_dist < self.discard_over:
+                if minimum_x_dist > self.max_dist: minimum_x_dist = self.max_dist
+                if self.debug: log['step_to_minimum'] = float(minimum_x_dist) # type:ignore
+
+                # make a step towards 0
+                all_params.sub_(all_grads, alpha = minimum_x_dist - self.lr)
+
+                # if validate_step is enabled, make sure loss didn't increase
+                if self.validate_step:
+                    y_validation = closure(backward=False).detach().cpu()
+                    if self.debug: log['y_validation'] = float(y_validation) # type:ignore
+                    # if it increased, undo the step
+                    if y_validation > y_step:
+                        all_params.add_(all_grads, alpha = minimum_x_dist - self.lr)
+
+        self.current_step += 1
         return y_step
